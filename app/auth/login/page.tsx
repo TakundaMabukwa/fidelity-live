e17b0d0d8b457cd8af86c99bd6d14655b404d1fa
TakundaMabukwa <mabukwatakunda@gmail.com>
2025-09-23 03:47:31 +0200

    added new bg image

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,13 @@
 import { LoginForm } from "@/components/login-form";
+import bg from "@/components/bg.jpg"
+
 
 export default function Page() {
   return (
     <div 
       className="relative flex justify-center items-center p-6 md:p-10 w-full min-h-svh"
       style={{
-        backgroundImage: "url('https://scontent-jnb2-1.xx.fbcdn.net/v/t39.30808-6/482266998_9344935868927595_6100885459771978650_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=0b6b33&_nc_ohc=HxPYQgdZUvAQ7kNvwGHYvJk&_nc_oc=Adkth1ayN_-9moWsQ_4zY1zeMPH41Bq72Im4qieLOaOhEAtsO2xZUdOk2aFMJnZKRBc&_nc_zt=23&_nc_ht=scontent-jnb2-1.xx&_nc_gid=YyZMKFy8yElapRYzyTSDlg&oh=00_AfaPinFgKKW8b6BkJrDrHGuVQK0Zwrb9Wr6_pUOZilB7IQ&oe=68C7A70A')",
+        backgroundImage: `url('${bg.src}')`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
